d9be96f8fdb5f0925c61e7cb68329b7272737de1
szymonhrehorowicz <szymhre@gmail.com>
2023-04-01 20:32:44 +0200

    Revert "solved problem 1"
    This reverts commit 0c5a9fd9ad2f677bede6c1fd4ad9100a89f43eb5.

--- problem1.c
+++ problem1.c
@@ -1,10 +1,3 @@
-#include "stdio.h"
 
-int main()
-{
-    long sumOfMultiples = 3 * sum((int)999 / 3) + 5 * sum((int)999 / 5) - 15 * sum((int)999 / 15);
-
-    printf("Sum of ")
-
-        return 0;
+return 0;
 }
